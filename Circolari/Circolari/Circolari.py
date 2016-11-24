1cdc0e658c68c16fa8c347fb0145c3a2bccd5842
Giuseppe Lonardoni <gogol.logog@hotmail.it>
2016-11-24 15:45:20 +0100

    ascii

--- Circolari/Circolari/Circolari.py
+++ Circolari/Circolari/Circolari.py
@@ -21,7 +21,7 @@ def main(TOKEN):
     if len(temp3) != 0:
         with open('archivio.txt', 'a') as archivio:
             for e in temp3:
-                archivio.write(e + "\n")
+                archivio.write(str(e) + "\n")
                 bot.sendMessage('163329729', e.replace("@n", "\n"), parse_mode='HTML')
                 bot.sendMessage('66441008', e.replace("@n", "\n"), parse_mode='HTML')
     else:
